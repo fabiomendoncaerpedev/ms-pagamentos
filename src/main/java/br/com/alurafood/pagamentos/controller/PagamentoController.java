ac30a498487e6274979be7c11bea454fa96842e2
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-07-12 15:01:57 -0300

    feat: creation of the payment message publisher

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -6,6 +6,8 @@ import br.com.alurafood.pagamentos.service.PagamentoService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +23,8 @@ import java.net.URI;
 public class PagamentoController {
     @Autowired
     private PagamentoService service;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @GetMapping
     public Page<PagamentoDto> listar(@PageableDefault(size = 10) Pageable paginacao) {
@@ -40,6 +44,9 @@ public class PagamentoController {
         PagamentoDto pagamento = service.criarPagamento(dto);
         URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
 
+        Message message = new Message(("Criei um pagamento com o id " + pagamento.getId()).getBytes());
+        rabbitTemplate.send("pagamento.concluido", message);
+        
         return ResponseEntity.created(endereco).body(pagamento);
     }
 
